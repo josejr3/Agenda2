580c4b8460e3ba9899010d9009e6aac037ee0c6d
josejr3 <gregorio.jr2@gmail.com>
2025-12-09 17:33:20 +0100

    agenda2

---
+++ agenda-macro-nutrinetes/database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+         User::create([
+            'name'=>'jose',
+            'password' => Hash::Make('jose'),
+            'email'=>'gregorio.jr2@gmail.com',
+        ]);
+    }
+}
---
+++ agenda-macro-nutrinetes/resources/views/dashboard.blade.php
@@ -0,0 +1,30 @@
+<?php
+require 'C:\Users\grego\Desktop\Agenda2\agenda-macro-nutrinetes\vendor\autoload.php';
+
+use StaticKidz\BedcaAPI\BedcaClient;
+
+$client = new BedcaClient();
+?>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+                <?php
+                        $client = new BedcaClient();
+                        $foodGroups = $client->getFoodGroups();
+                        print_r($foodGroups);
+                        $food = $client->getFood(893);
+                        //var_dump($food);
+                    ?>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                  
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
